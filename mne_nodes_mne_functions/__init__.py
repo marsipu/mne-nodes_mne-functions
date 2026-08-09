7455ea3ac4fc7189340f25909ed842ba73ff3c9b
Martin G. Schulz <46245704+marsipu@users.noreply.github.com>
2026-08-09 18:01:21 +0200

    placeholder script-function

--- mne_nodes_mne_functions/__init__.py
+++ mne_nodes_mne_functions/__init__.py
@@ -1,6 +1,6 @@
-from importlib.resources import files
+from pathlib import Path
 
 PLUGIN_NAME = "mne-functions"
 PLUGIN_GITHUB = "https://github.com/marsipu/mne-nodes_mne-functions"
-CONFIG_PATH = files(__package__) / "mne_functions_config.json"
-SCRIPT_PATH = None
+CONFIG_PATH = Path(__file__).parent / "mne_functions_config.json"
+SCRIPT_PATH = Path(__file__).parent / "mne_functions.py"
